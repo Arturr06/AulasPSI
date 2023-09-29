d278e2057c77806bc875a6e86dacffbab8f126ce
Artur <arturpina400@gmail.com>
2023-09-29 10:38:38 +0100

    Criar as variáveis inteiras

---
+++ Semana01/TipoVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TipoVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 5;
+            int valor2 = 12;
+            int valor3 = 1;
+
+            Console.WriteLine(valor1, valor2, valor3);
+
+        }
+    }
+}
