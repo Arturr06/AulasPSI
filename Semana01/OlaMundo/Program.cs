9f6beee8c775eb91de078ca8bafeca2d0e888996
Artur <arturpina400@gmail.com>
2023-09-29 10:14:30 +0100

    Comentar o programa.

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -2,9 +2,16 @@
 
 namespace OlaMundo
 {
-    ///class Program
+    /// <summary>
+    /// Este programa mostra a frase "Ola Mundo!" na consola.
+    /// </summary>
+    class Program
     {
-        static void ///Main(string[] args)
+        /// <summary>
+        /// Este é o metodo que faz com que a frase "Ola Mundo!" apareça na consola."
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
