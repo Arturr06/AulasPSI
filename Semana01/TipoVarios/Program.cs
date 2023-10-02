f2a529bcd760d80c9d1fb1eecd9b3c4e0f321f86
Artur <arturpina400@gmail.com>
2023-10-02 08:35:48 +0100

    Fazer o programa tipos varios

--- Semana01/TipoVarios/Program.cs
+++ Semana01/TipoVarios/Program.cs
@@ -7,10 +7,27 @@ namespace TipoVarios
         static void Main(string[] args)
         {
             int valor1 = 5;
-            int valor2 = 12;
-            int valor3 = 1;
+            long valor2 = 2000000;
+            short valor3 = 1;
 
             Console.WriteLine(valor1, valor2, valor3);
+
+            double real1 = 3.422;
+            decimal real2 = 15.322;
+            float real3 = 12.512f;
+
+            Console.WriteLine(real1, real2, real3);
+
+             char simboloCopyright = '\u00A9';
+             char heart = '\u2665';
+
+            Console.WriteLine(simboloCopyright, heart);
+
+            bool b = true;
+            bool c = 4 > 5;
+
+            Console.WriteLine(b, c);
+         
 
         }
     }
