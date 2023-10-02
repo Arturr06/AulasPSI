9119d91037718da299560d22754701dfc3a4d6e9
Artur <arturpina400@gmail.com>
2023-10-02 09:00:22 +0100

    Fazer o programa VariasStrings

---
+++ Semana02/VariasStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Artur \u00A9";
+            
+            string pasta = "C:\\Users\\nome\\Documents";
+            
+            string pasta2 = @"C\Users\nome\Documents";
+            
+            string melhor = "\t Ronaldo \u0031"
+
+            Console.WriteLine(nome);
+            Console.WriteLine(pasta);
+            Console.WriteLine(pasta2);
+            Console.WriteLine(melhor);
+        }
+    }
+}
