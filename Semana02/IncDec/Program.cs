91de78173a2cb6013bded77adfd72a789a209b80
Artur <arturpina400@gmail.com>
2023-10-04 08:38:08 +0100

    Utilizar o prefixo no prograga IncDec

---
+++ Semana02/IncDec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sufxA = 3;
+            int sufxB = 5;
+            int c = 0;
+
+            int prefA = 7;
+            int prefB = 5;
+            int c2 = 0;
+
+
+            c = sufxA++ + sufxB++;
+            c2 = --prefA - --prefB;
+
+            
+            Console.WriteLine(c);
+            Console.WriteLine();
+            Console.WriteLine(c2);
+        }
+    }
+}
