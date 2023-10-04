ea4bd326a58dd14e63ebc81d082e91243eed9849
Artur <arturpina400@gmail.com>
2023-10-04 09:21:30 +0100

    Colocar as contas

--- Semana02/Cilindro/Program.cs
+++ Semana02/Cilindro/Program.cs
@@ -10,10 +10,13 @@ namespace Cilindro
             double raio;
             double volume;
             double AreaS;
-            
+            double pi = 3.1415926;
+
             string texto = Console.ReadLine();
 
-            
+            volume = pi * raio * altura;
+
+            AreaS = (2 * pi) * raio * (raio + altura);
 
         }
     }
