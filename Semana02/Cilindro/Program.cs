d1d9db555cc0648556255cfa930f925e655f4d85
Artur <arturpina400@gmail.com>
2023-10-04 09:18:38 +0100

    Criar o programa Cilindro e adicionar as variaveis

---
+++ Semana02/Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            double raio;
+            double volume;
+            double AreaS;
+            
+            string texto = Console.ReadLine();
+
+            
+
+        }
+    }
+}
