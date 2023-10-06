da088abfce1b0d087ef522b914a6cfaaa54aa655
Artur <arturpina400@gmail.com>
2023-10-06 10:08:13 +0100

    Adicionar outros valores especiais e testar a divisão por 0

--- Semana02/ValoresEspeciais/Program.cs
+++ Semana02/ValoresEspeciais/Program.cs
@@ -6,12 +6,15 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
+            int a = 3;
             Console.WriteLine(int.MaxValue);
             Console.WriteLine(double.MinValue);
             Console.WriteLine(float.PositiveInfinity);
             Console.WriteLine(double.NegativeInfinity);
             Console.WriteLine(double.NaN);    
-
+            Console.WriteLine((double)float.MaxValue);
+            Console.WriteLine(float.NaN);
+            Console.WriteLine(a / 0);
         }
     }
 }
