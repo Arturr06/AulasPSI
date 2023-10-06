115be385e96adad51d59359dd569239e81ad57e8
Artur <arturpina400@gmail.com>
2023-10-06 10:28:12 +0100

    Utilizar overflow com numeros inteiros e usar overflow e underflow com numeros reais

--- Semana02/ValoresEspeciais/Program.cs
+++ Semana02/ValoresEspeciais/Program.cs
@@ -6,7 +6,12 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            int a = 3;
+            int b = 5;
+            int c = 15;
+            float f1, f2;
+
+            f1 = f2 = 13200.0f;
+
             Console.WriteLine(int.MaxValue);
             Console.WriteLine(double.MinValue);
             Console.WriteLine(float.PositiveInfinity);
@@ -14,7 +19,11 @@ namespace ValoresEspeciais
             Console.WriteLine(double.NaN);    
             Console.WriteLine((double)float.MaxValue);
             Console.WriteLine(float.NaN);
-            Console.WriteLine(a / 0);
+            Console.WriteLine((double) (b + 1));
+            Console.WriteLine((short) (c - 1));
+            Console.WriteLine(2 * double.MaxValue);
+            Console.WriteLine(float.MinValue / 2);
+            Console.WriteLine(f1 == f2 + 0.00001f); 
         }
     }
 }
