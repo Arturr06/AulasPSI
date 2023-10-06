f7899ea41d16a17ec8db1ec68acfb14ca4b096ed
Artur <arturpina400@gmail.com>
2023-10-06 09:01:46 +0100

    Adicionar funções e constantes matematicas

--- Semana02/Cilindro/Program.cs
+++ Semana02/Cilindro/Program.cs
@@ -6,18 +6,30 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            double altura;
-            double raio;
+            double altura; 
+            double raio; 
+            
+            Console.WriteLine("Insira o valor da altura. ");
+            string texto = Console.ReadLine();
+            Console.WriteLine("Insira o valor do raio. ");
+            string texto2 = Console.ReadLine();
+
+            altura = double.Parse(texto);
+            raio = double.Parse(texto2);
+
             double volume;
             double AreaS;
-            double pi = 3.1415926;
+            //double pi = 3.1415926;
+            
+            volume = Math.PI * Math.Pow(raio, raio) * altura;
 
-            string texto = Console.ReadLine();
+            AreaS = 2 * Math.PI * raio * (raio + altura);
 
-            volume = pi * raio * altura;
-
-            AreaS = (2 * pi) * raio * (raio + altura);
-
+            Console.WriteLine("O valor do volume é: ");
+            Console.WriteLine(volume);
+            Console.WriteLine();
+            Console.WriteLine("O valor da área de superficie: ");
+            Console.WriteLine(AreaS);
         }
     }
 }
