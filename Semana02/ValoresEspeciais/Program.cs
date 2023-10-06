03f3aa82ce8eb2ed2e24db5824156423cfb06f45
Artur <arturpina400@gmail.com>
2023-10-06 09:35:02 +0100

    Fazer o exercicio 9

---
+++ Semana02/ValoresEspeciais/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max = max.MaxValue;
+            double min = min.MinValue;
+
+            double infpositivo = infpositivo.PositiveInfinity;
+            double infnegativo = infnegativo.NegativeInfinity;
+            float nrinvalido = nrinvalido.NaN;
+
+
+
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            Console.WriteLine(infpositivo);
+            Console.WriteLine(infnegativo);
+            Console.WriteLine(nrinvalido);    
+
+        }
+    }
+}
