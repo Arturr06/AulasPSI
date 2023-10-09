3135264cf8f2b0db9e152ccb10387d81ebb972a7
Artur <arturpina400@gmail.com>
2023-10-09 09:26:51 +0100

    Fazer exercicios com if/else if

---
+++ Semana02/Skynet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Skynet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pergunta = Console.ReadLine();
+            string resposta1 = "Benfica";
+            string resposta2 = "Lisboa";
+            string resposta3 = "Russia";
+            
+
+            if(pergunta == "Qual o melhor clube do mundo?"){
+                Console.WriteLine(resposta1);
+            }else if(pergunta == "Qual é a capital de Portugal?"){
+                Console.WriteLine(resposta2);
+            }else if(pergunta == "Qual é o maior pais do mundo?"){
+                Console.WriteLine(resposta3);
+            }else {
+                Console.WriteLine("Não tenho resposta para isso.");
+            }
+        }
+    }
+}
