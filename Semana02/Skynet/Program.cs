020cd10514e39c0e435d50d5773af620561abc24
Artur <arturpina400@gmail.com>
2023-10-09 10:20:44 +0100

    Modificar o programa Skynet substituir o ciclo

--- Semana02/Skynet/Program.cs
+++ Semana02/Skynet/Program.cs
@@ -12,7 +12,7 @@ namespace Skynet
             string resposta3 = "Russia";
             
 
-            if(pergunta == "Qual o melhor clube do mundo?"){
+            /*if(pergunta == "Qual o melhor clube do mundo?"){
                 Console.WriteLine(resposta1);
             }else if(pergunta == "Qual é a capital de Portugal?"){
                 Console.WriteLine(resposta2);
@@ -20,7 +20,25 @@ namespace Skynet
                 Console.WriteLine(resposta3);
             }else {
                 Console.WriteLine("Não tenho resposta para isso.");
+            }*/
+
+            switch (pergunta)
+            {
+                case "Qual o melhor clube do mundo?" : 
+                    Console.WriteLine(resposta1);
+                    break;
+                case "Qual é a capital de Portugal?" :
+                    Console.WriteLine(resposta2);
+                    break;
+                case "Qual é o maior pais do mundo?" :
+                    Console.WriteLine(resposta3);
+                    break;        
+                default:
+                    Console.WriteLine("Não tenho resposta para isso.");
+                    break;
             }
+
+
         }
     }
 }
