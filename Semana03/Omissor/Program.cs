b7d1ac72df4a535087e6fb9a14d3293c53b12d27
Artur <arturpina400@gmail.com>
2023-10-13 09:03:45 +0100

    Fazer exercicio a utilizar arrays

---
+++ Semana03/Omissor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Omissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa deve solicitar uma string ao utilizador
+            Console.WriteLine("Insira uma frase.");
+
+            string frase = Console.ReadLine();
+
+            //Seguidamente, deve solicitar um caráter ao utilizador
+            Console.WriteLine("Insira um carater que quer que seja removido da frase.");
+
+            string caraterint = Console.ReadLine();
+
+            char carater = caraterint[0]; 
+
+            //Por fim, deve imprimir a string omitindo o caráter
+
+            foreach (char c in frase) {
+
+                if(c != carater)
+                {
+                    Console.Write(c);
+                }
+            }
+                
+               
+
+            
+        }
+    }
+}
---
+++ Semana03/Piramide/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("Introduza o número de linhas.");
+            string nrlinhas = Console.ReadLine();
+
+            y = Convert.ToInt32(nrlinhas);
+
+            for (int i = 1; i <= y; i++)
+                {
+                        for(int a = 1; a <= y - i; a++){
+                            Console.Write(" ");
+                        }
+
+                        for(int b = 1; b <= 2 * i - 1; b++){
+                            Console.Write("*");
+                        }
+
+                    Console.WriteLine();     
+                }
+        }
+    }
+}
