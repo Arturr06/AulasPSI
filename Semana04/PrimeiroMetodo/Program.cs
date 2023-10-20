96bf27a9e824908d89bb1e4efed9d70ca1266612
Artur <arturpina400@gmail.com>
2023-10-20 10:28:19 +0100

    Fazer exercicio 3 semana 4

--- Semana04/PrimeiroMetodo/Program.cs
+++ Semana04/PrimeiroMetodo/Program.cs
@@ -6,9 +6,24 @@ namespace Primeiro_Metodo
     {
         static void Main(string[] args)
         {
-           ContarAte10();
-           Console.WriteLine();
-           ContarAte10(); 
+           //ContarAte10();
+           //Console.WriteLine();
+           //ContarAte10();
+
+            int N;          
+           Console.WriteLine("Introduza o número. ");
+           string Nr = Console.ReadLine();
+
+           N = Convert.ToInt32(Nr);
+
+           ContarAteN(N);
+          
+           Console.WriteLine("Introduza o número. ");
+           Nr = Console.ReadLine();
+
+           N = Convert.ToInt32(Nr);
+
+           ContarAteN(N); 
         }
 
         private static void ContarAte10()
@@ -16,6 +31,15 @@ namespace Primeiro_Metodo
             for(int i = 1; i <= 10; i++)
             {
                Console.WriteLine(i); 
+            }
+        }
+
+        private static void ContarAteN(int N)
+        {
+
+            for(int o = 1; o <= N; o++)
+            {
+                Console.WriteLine(o);
             }
         }
     }
