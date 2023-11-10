033de0f8f8f918547b319466a74f585d7f4b0c20
Artur <arturpina400@gmail.com>
2023-11-10 09:20:58 +0000

    Fazer exercicio 1

---
+++ Semana06/OMeuJogo/Inimigo.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace OMeuJogo
+{
+    public class Inimigo
+    {
+        
+        private string nome;
+        private int vida;  
+        
+        public Inimigo(string nome)
+        {        
+            this.nome = nome;
+            vida = 100;
+        }
+
+    }
+}
