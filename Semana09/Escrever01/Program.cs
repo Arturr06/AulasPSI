c1dce41ded65ea1663fd13aeeeffc90c87dfb6a9
Artur <arturpina400@gmail.com>
2024-01-08 08:44:14 +0000

    Fazer exercicio 3 da semana 9

---
+++ Semana09/Escrever01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Escrever01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[1000];
+            int b = 0;
+
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktop, "File.txt");
+
+            Console.WriteLine("Escreva o que pretende adicionar ao ficheiro.");
+            Console.WriteLine("");
+
+            do
+            {
+                a[b] = Console.ReadLine();
+
+                b++;
+            }while(a[b - 1] != "");
+
+            File.WriteAllLines(filePath, a);
+            
+
+        }
+    }
+}
---
+++ Semana09/Escrever02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Escrever02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[1000];
+            int b = 0;
+            
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktop, "File.txt");
+            StreamWriter dd = new StreamWriter(filePath);
+
+
+            Console.WriteLine("Escreva o que pretende adicionar ao ficheiro.");
+            Console.WriteLine("");
+
+            do
+            {
+                a[b] = Console.ReadLine();
+
+                if(a[b] != "")
+                {
+                    dd.WriteLine(a[b]);
+                }
+                
+                b++;
+              
+            }while(a[b - 1] != "");            
+            
+            dd.Close();
+        }
+    }
+}
