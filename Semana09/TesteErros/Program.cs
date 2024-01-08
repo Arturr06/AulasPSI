632ecac0b5df73952213e91f23cabb10b5126a55
Artur <arturpina400@gmail.com>
2024-01-08 08:55:15 +0000

    Fazer exercicio 4

--- Semana09/TesteErros/Program.cs
+++ Semana09/TesteErros/Program.cs
@@ -6,13 +6,20 @@ namespace TesteErros
     {
         static void Main(string[] args)
         {
+            int i = 0;
             
             Console.Write("Insere um número inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
+            
+            try
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte problema:" + e.Message);
+            }
+            
             Console.WriteLine($"Número inserido: {i}");
-
-
-
         }
     }
 }
