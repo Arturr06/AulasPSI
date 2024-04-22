ed5a9f58437b374c4cc58af8573ef26d69cbbe2b
Artur <arturpina400@gmail.com>
2024-04-22 10:09:20 +0100

    Fazer exercicio 2

--- Semana14/LINQLeitor/Program.cs
+++ Semana14/LINQLeitor/Program.cs
@@ -18,21 +18,30 @@ namespace LINQLeitor
 
             lerlinhas = File.ReadAllLines(filePath);
 
-            int nrlinhas = lerlinhas.Count(x => x.Length > 30);
+            int nrlinhas = 
+                (from x in lerlinhas
+                where x.Length > 30
+                select x).Count();
 
             Console.WriteLine("Número de linhas lidas com mais de 30 caracteres: " + nrlinhas);
 
-            mediacarateres = lerlinhas.Average(x => x.Length);
+            mediacarateres = 
+                (from x in lerlinhas
+                select x.Length).Average();
 
             Console.WriteLine("Media do número de caracteres das linhas lidas: " + mediacarateres);
 
-            linha120 = lerlinhas.Any(x => x.Length > 120);
+            linha120 = 
+                (from x in lerlinhas
+                where x.Length > 120
+                select x).Any();
 
             Console.WriteLine("Existe alguma linha com mais que 120 caracteres? " + linha120);
 
-            IEnumerable<string> primeirapalavra = lerlinhas
-                .Where(x => x.Contains("Y"))
-                .Select(x => x.Split(delimitador)[0]);
+            IEnumerable<string> primeirapalavra = 
+                from x in lerlinhas
+                where x.Contains("Y")
+                select x.Split(delimitador)[0];
 
             foreach (string ax in primeirapalavra)
             {
